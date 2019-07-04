dd1135ca0a82f90698ca03aff5dc89a0f0caa6aa
zhengliu <zheng.liu@aihuishou.com>
2019-07-04 19:02:49 +0800

    商品分类修改

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -22,6 +22,11 @@ class _CategoryPageState extends State<CategoryPage> {
           child: Row(
             children: <Widget>[
               LeftCategoryNav(),
+              Column(
+                children: <Widget>[
+                  RightCategoryNav(),
+                ],
+              )
             ],
           ),
         ),
@@ -92,5 +97,51 @@ class _LeftCategoryNavState extends State<LeftCategoryNav> {
       });
 //      categoryModelResp.data.forEach((i) => print(i.mallCategoryName));
     });
+  }
+}
+
+class RightCategoryNav extends StatefulWidget {
+  @override
+  _RightCategoryNavState createState() => _RightCategoryNavState();
+}
+
+class _RightCategoryNavState extends State<RightCategoryNav> {
+  List list = ['密码', '手动阀', 'fasdf', '阿斯蒂芬', '方式', '发', '密码', '手动阀', 'fasdf'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(80),
+      width: ScreenUtil().setWidth(570),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1, color: Colors.black12),
+          )),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          return _rightInkWell(list[index].toString());
+        },
+      ),
+    );
+  }
+
+  Widget _rightInkWell(String item) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+        child: Center(
+          child: Text(
+            item,
+            style: TextStyle(
+              fontSize: ScreenUtil().setSp(28),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
